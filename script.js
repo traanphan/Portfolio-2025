fb28a1a92ba42e13eb0a59761cce72def8ec5ad2
Tran Phan <phanbaotran0501@gmail.com>
2025-04-22 20:20:58 -0400

    Added slide up animation

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".slide-up");
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    }, {
+      threshold: 0.2
+    });
+  
+    elements.forEach(el => observer.observe(el));
+  });
+  
